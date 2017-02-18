707ba9de93f078055eeaa79e1ebb4daa83376929
subash karki <skarki35205@hotmail.com>
2017-02-18 17:07:26 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,14 @@
+
+function forLoop(array) {
+    //     for(let i = 0; i < 25; i++) {
+    //         if (i === 1) {
+    //             console.log(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
+    //         }else  {
+    //             console.log(`I am ${i} strange loops.`);
+    //
+    //         }
+    // }
+    for (let i = 0, l = testArray.length; i < l; i++) {
+      let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+}
+forLoop();
